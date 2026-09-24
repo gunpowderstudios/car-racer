5c7826c19545c236dd08c7affc7c971de161ea89
Gunpowder <tim@gunpowderstudios.co.uk>
2026-09-24 12:17:39 +0100

    stamp

---
+++ workflows/version.js
@@ -0,0 +1,4 @@
+// Shown small on the menu screen. VERSION is edited by hand; BUILD is stamped automatically
+// by .github/workflows/stamp.yml on every push to main (it commits the short commit id + date).
+export const VERSION = '12.4';
+export const BUILD = 'dev';
